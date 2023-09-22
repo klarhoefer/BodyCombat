bbd9553a68219fb5bd35517a3db6bf03491c6303
Udo Klarhöfer <klarhoefer@gmail.com>
2023-09-22 21:25:15 +0200

    Display by track number

--- src/View.elm
+++ src/View.elm
@@ -1,29 +1,58 @@
 module View exposing (view)
 
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 
 import Common exposing (..)
 
+
 view : Model -> Html Msg
 view model =
     div []
-        ((viewError model.errMsg) ++ (viewTracks model.tracks))
+        [
+            (case model.errMsg of
+                Just err ->
+                    div [] [ text err ]
+                Nothing ->
+                    div []
+                        [ div []
+                            (viewTracks model)
+                        ]
+            )
+        ]
 
-viewError : Maybe String -> List (Html Msg)
-viewError err =
-    case err of
-        Just msg -> [ div [] [ text msg ]]
-        Nothing -> []
+
+viewTracks : Model -> List (Html Msg)
+viewTracks model =
+    List.range 1 10
+    |> List.map (\n -> viewTrackNumber n model)
 
 
-viewTracks : List Track -> List (Html Msg)
-viewTracks tracks =
-    List.map viewTrack tracks
+viewTrackNumber : Int -> Model -> Html Msg
+viewTrackNumber n model =
+    let
+        tracks =
+            List.filter (\t -> t.track == n) model.tracks
+            |> List.sortBy (\t -> -t.release)
+    in
+        div []
+            [ h4 [] [ text <| "Track #" ++ (String.fromInt n) ]
+            , div [ class "tracksN" ]
+                (List.map viewTrack tracks)
+            ]
+
+releaseName : Float -> String
+releaseName r =
+    if r == 84.5 then
+        "United"
+    else
+        String.fromFloat r
 
 
 viewTrack : Track -> Html Msg
 viewTrack track =
     div []
-        [ text track.artist ]
+        [ text <| (releaseName track.release) ++ " " ++ track.artist ++ "/" ++ track.title 
+        ]
