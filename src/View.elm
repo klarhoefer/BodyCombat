0176d224f6d107258f0f09fef20cf18984281463
Udo Klarhöfer <klarhoefer@gmail.com>
2023-09-24 15:47:17 +0200

    Convert to tables

--- src/View.elm
+++ src/View.elm
@@ -8,6 +8,38 @@ import Html.Events exposing (..)
 import Common exposing (..)
 
 
+releaseName : Float -> String
+releaseName r =
+    if r == 84.5 then
+        "Utd"
+    else
+        String.fromFloat r
+
+
+nth : Int -> List x -> Maybe x
+nth n l =
+    case l of
+        [] -> Nothing
+        h :: t ->
+            if n == 1 then
+                Just h
+            else
+                nth (n - 1) t
+
+
+minutes : Maybe Int -> String
+minutes seconds =
+    case seconds of
+        Nothing -> "-.--"
+        Just n ->
+            let
+                secs = modBy 60 n
+                mins = (n - secs) // 60
+                pad = if secs < 10 then "0" else ""
+            in
+                (String.fromInt mins) ++ ":" ++ pad ++ (String.fromInt secs)
+
+
 view : Model -> Html Msg
 view model =
     div []
@@ -19,8 +51,8 @@ view model =
                     div [ class "outer" ]
                         [ table []
                             ((tr []
-                                [ th [] [ text "#" ]
-                                , th [] [ text "Title" ]
+                                [ th [ style "text-align" "right" ] [ text "#" ]
+                                , th [ colspan 3 ] [ text "Title" ]
                                 ]
                              ) :: (viewTracks model)
                             )
@@ -35,17 +67,6 @@ viewTracks model =
     |> List.map (\n -> viewTrackNumber n model)
 
 
-nth : Int -> List x -> Maybe x
-nth n l =
-    case l of
-        [] -> Nothing
-        h :: t ->
-            if n == 1 then
-                Just h
-            else
-                nth (n - 1) t
-
-
 viewTrackNumber : Int -> Model -> Html Msg
 viewTrackNumber n model =
     let
@@ -54,35 +75,31 @@ viewTrackNumber n model =
             |> List.sortBy (\t -> -t.release)
         selected = nth n model.selected |> Maybe.withDefault defaultTrack
     in
-        tr []
-            [ td [ style "text-align" "right" ] [ text <| String.fromInt n ]
-            , if model.open /= n then
-                td [] [viewSelectedTrack selected n]
+        tr (if model.open /= n then [ onClick (SelectedClicked n) ] else [])
+            <| (td [ style "text-align" "right" ] [ text <| String.fromInt n ])
+            ::
+            (if model.open /= n then
+                (viewSingleTrack selected)
               else
-                td []
+                [ td [ colspan 3 ]
                     [ div [ class "tracksN" ]
-                        (List.map viewTrack tracks)
+                        [ table []
+                            (List.map viewTrack tracks)
+                        ]
                     ]
-            ]
+                ]
+            )
 
+viewSingleTrack : Track -> List (Html Msg)
+viewSingleTrack track =
+    [ td [ style "text-align" "right" ] [ text <| releaseName track.release ]
+    , td [] [ text track.title ]
+    , td [ style "text-align" "right" ] [ text <| minutes track.seconds]
+    ]
 
-releaseName : Float -> String
-releaseName r =
-    if r == 84.5 then
-        "United"
-    else
-        String.fromFloat r
 
 
 viewTrack : Track -> Html Msg
 viewTrack track =
-    div [ onClick (TrackClicked track) ]
-        [ text <| (releaseName track.release) ++ " " ++ track.artist ++ "/" ++ track.title 
-        ]
-
-
-viewSelectedTrack : Track -> Int -> Html Msg
-viewSelectedTrack track n =
-    div [ onClick (SelectedClicked n) ]
-        [ text <| (releaseName track.release) ++ " " ++ track.artist ++ "/" ++ track.title 
-        ]
+    tr [ onClick (TrackClicked track) ]
+        (viewSingleTrack track)
